69192fd74811441d86fa8aeb0a13a462ceecb756
umerfarooq6699 <umer66997@gmail.com>
2024-10-13 10:15:55 -0700

    new changes added

--- Controllers/dashboardControllers.js
+++ Controllers/dashboardControllers.js
@@ -2,49 +2,99 @@ const productSchema = require("../Models/productSchema")
 const userSchema = require("../Models/userSchema")
 const ApiResponse = require("../Utilities/ApiResponse")
 
-const productadd=async(req,res)=>{
+const productadd = async (req, res) => {
+    try {
+        await productSchema.create(req.body);
+        res.send(new ApiResponse("success", "Product added successfully"));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to add product")); // Error response bhejna
+    }
+};
 
-    console.log(req.body,"product object controller")
-    await productSchema.create(req.body)
-    res.send(new ApiResponse("success","Product added successfully"))
-}
 
+const getCollection = async (req, res) => {
+    try {
+        const data = await productSchema.find();
+        res.send(new ApiResponse("success", "Products deleted successfully", { array: data }));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to retrieve products")); // Error response bhejna
+    }
+};
 
-const getCollection=async(req,res)=>{
-    const data=await productSchema.find()
-    // console.log(data,"backend deata")
-    res.send(new ApiResponse("success","Product deleted successfully",{array:data}))
-}
+const getSingleProduct = async (req, res) => {
+    console.log(req.body, "Body jjjjjjjjjjjjjjj");
+    try {
+        const product = await productSchema.findOne({ _id: req.body.id });
+        if (!product) {
+            return res.status(404).send(new ApiResponse("error", "Product not found")); // Agar product nahi mila to error response bhejna
+        }
+        res.send(new ApiResponse("success", "Data fetched successfully", { object: product }));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to fetch product")); // Error response bhejna
+    }
+};
 
-const getSingleProduct=async(req,res)=>{
-    console.log(req.body,"Body jjjjjjjjjjjjjjj")
-    const product=await productSchema.findOne({_id:req.body.id})
-    res.send(new ApiResponse("success","Data fetched successfully",{object:product}))
-}
+const updateProduct = async (req, res) => {
+    try {
+        const data = await productSchema.findOneAndUpdate(
+            { _id: req.body.id },
+            { $set: req.body.obj },
+            { new: true }
+        );
 
-const updateProduct=async(req,res)=>{
-    // console.log(req.body,"update object body")
-    const data=await productSchema.findOneAndUpdate({_id:req.body.id},{$set:req.body.obj},{new:true})
-    res.send(new ApiResponse("success","Product updated successfully"))
-    // console.log(data,"updated")
-}
+        if (!data) {
+            return res.status(404).send(new ApiResponse("error", "Product not found")); // Agar product nahi mila to error response bhejna
+        }
 
-const dashboardDeleteProduct=async(req,res)=>{
-    await productSchema.findOneAndDelete({_id:req.body._id})
-    // console.log(req.body,"dashboardDeleteProduct")
-    // const allProducts=await productSchema.find()
-    res.send(new ApiResponse("success","Product deleted successfully",))
-}
+        res.send(new ApiResponse("success", "Product updated successfully", { object: data }));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to update product")); // Error response bhejna
+    }
+};
 
-const getAllUsers=async(req,res)=>{
-    const data=await userSchema.find()
-    res.send(new ApiResponse("success","User Fetched successfully",{array:data}))
-}
+const dashboardDeleteProduct = async (req, res) => {
+    try {
+        const deletedProduct = await productSchema.findOneAndDelete({ _id: req.body._id });
 
-const deleteDashboardUser=async(req,res)=>{
-    console.log(req.body,"deleteDashboardUser")
-    await userSchema.findOneAndDelete({_id:req.body.id})
-    res.send(new ApiResponse("success","User deleted successfully"))
-}
+        if (!deletedProduct) {
+            return res.status(404).send(new ApiResponse("error", "Product not found")); // Agar product nahi mila to error response bhejna
+        }
+
+        res.send(new ApiResponse("success", "Product deleted successfully"));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to delete product")); // Error response bhejna
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const data = await userSchema.find();
+        res.send(new ApiResponse("success", "Users fetched successfully", { array: data }));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to fetch users")); // Error response bhejna
+    }
+};
+
+const deleteDashboardUser = async (req, res) => {
+    try {
+        console.log(req.body, "deleteDashboardUser");
+        const deletedUser = await userSchema.findOneAndDelete({ _id: req.body.id });
+        
+        if (!deletedUser) {
+            return res.status(404).send(new ApiResponse("error", "User not found"));
+        }
+        
+        res.send(new ApiResponse("success", "User deleted successfully"));
+    } catch (error) {
+        console.error(error); // Error ko console par log karna
+        res.status(500).send(new ApiResponse("error", "Failed to delete user")); // Error response bhejna
+    }
+};
 
 module.exports={productadd,getCollection,getSingleProduct,updateProduct,dashboardDeleteProduct,getAllUsers,deleteDashboardUser}
--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -5,54 +5,77 @@ const createToken = require("../Utilities/createToken")
 const hashPassword = require("../Utilities/encryptPassword")
 
 const signupController = async (req, res) => {
-    const bcrypted = await hashPassword(req.body.password)
-    console.log(bcrypted, "bcrypted")
-    req.body.password = bcrypted
-    await userSchema.create(req.body)
-    res.send(new ApiResponse("success", "Account created successfully"))
-}
+    try {
+        const bcrypted = await hashPassword(req.body.password);
+        req.body.password = bcrypted;
+        
+        await userSchema.create(req.body);
+        res.send(new ApiResponse("success", "Account created successfully"));
+    } catch (error) {
+        console.error(error); 
+        res.status(500).send(new ApiResponse("error", "Failed to create account")); 
+    }
+};
 
 
 const signinController = async (req, res) => {
-    console.log(req.body, "Signin backend")
-    const existingUser = await userSchema.findOne({ email: req.body.email })
-    console.log(existingUser, "existingUser")
+    try {
+        console.log(req.body, "Signin backend");
+        const existingUser = await userSchema.findOne({ email: req.body.email });
+        console.log(existingUser, "existingUser");
 
-    if (!existingUser) {
-        return res.send(new ApiResponse("error", "Invalid Email"))
+        if (!existingUser) {
+            return res.send(new ApiResponse("error", "Invalid Email"));
+        }
+
+        const matchedPassword = await comparePassword(req.body.password, existingUser.password);
+        console.log(matchedPassword, "matchedPassword");
+
+        if (matchedPassword) {
+            const Token = createToken(existingUser._id);
+            console.log(Token, "Token");
+            res.send(new ApiResponse("success", "Login", { user: existingUser, token: Token }));
+        } else {
+            res.send(new ApiResponse("error", "Wrong Password"));
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(new ApiResponse("error", "Failed to sign in")); 
     }
+};
 
-    const matchedPassword = await comparePassword(req.body.password, existingUser.password)
-    console.log(matchedPassword, "matchedPassword")
-
-    if (matchedPassword) {
-        const Token = createToken(existingUser._id)
-        console.log(Token, "Token")
-        res.send(new ApiResponse("success", "Login", { user: existingUser, token: Token }))
-    } else {
-        res.send(new ApiResponse("error", "Wrong Password"))
-    }
-
-}
 
 
 const changingPassword = async (req, res) => {
-    console.log(req.body, req.user, "ooooooooooooooooooooo")
-    const matchedPassword = await comparePassword(req.body.oldPassword, req.user.password)
-    if (!matchedPassword) {
-        return res.send(new ApiResponse("error", "Old password incorrect"))
-    }
-    console.log(matchedPassword, "matchedPassword")
-    const encodedPassword = await hashPassword(req.body.confirmPassword)
-    console.log(encodedPassword, "encodedPassword")
+    try {
 
-    const updatePassword=await userSchema.findOneAndUpdate({ _id: req.user._id }, { $set: { password: encodedPassword } }, { new: true })
-    if(updatePassword){
-        res.send(new ApiResponse("success","Password updated successfully"))
-    }else{
-        res.send(new ApiResponse("error","Failed to update password"))
+        const matchedPassword = await comparePassword(req.body.oldPassword, req.user.password);
+        if (!matchedPassword) {
+            return res.send(new ApiResponse("error", "Old password incorrect"));
+        }
+
+        console.log(matchedPassword, "matchedPassword");
+
+        const encodedPassword = await hashPassword(req.body.confirmPassword);
+        console.log(encodedPassword, "encodedPassword");
+
+        const updatePassword = await userSchema.findOneAndUpdate(
+            { _id: req.user._id },
+            { $set: { password: encodedPassword } },
+            { new: true }
+        );
+
+        if (updatePassword) {
+            res.send(new ApiResponse("success", "Password updated successfully"));
+        } else {
+            res.send(new ApiResponse("error", "Failed to update password"));
+        }
+    } catch (error) {
+        console.error(error); 
+        res.status(500).send(new ApiResponse("error", "Failed to change password"));
     }
-}
+};
+
 
 const tokenController=(req,res)=>{
     console.log(req.body,"TTTTTTTTTTTTTTTTTTTTTTTTTTTTt")
